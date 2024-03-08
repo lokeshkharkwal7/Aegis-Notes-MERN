6a97d110bf52ee029625356d1bba69a207606ac6
Lokesh Kharkwal <115694554+lokeshkharkwal7@users.noreply.github.com>
2024-03-08 19:13:01 +0530

    Update index.js
    ready to deploy on vercel

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ var cors = require('cors')
 const app = express();
 app.use(express.json())
 app.use(cors({
-  origin: [" https://aegis-notes-mern-frontend.vercel.app"],
+  origin: ["https://aegis-notes-mern-frontend.vercel.app"],
   // origin: ["https://wizstore.vercel.app"],
  
     methods: ["GET", "POST", "PUT", "DELETE"],
