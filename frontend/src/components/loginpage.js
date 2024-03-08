2376adee2fc563a473fb25505ea6cc8961386097
Lokesh Kharkwal <115694554+lokeshkharkwal7@users.noreply.github.com>
2024-03-08 19:19:19 +0530

    Update loginpage.js
    ready to deploy on vercel

--- frontend/src/components/loginpage.js
+++ frontend/src/components/loginpage.js
@@ -29,7 +29,7 @@ function Loginpage() {
     try {
       const response = await fetch(`${host}/auth/login`, {
         method: "POST",
-        mode: "cors",
+       
         headers: {
           "Content-Type": "application/json",
           // 'Content-Type': 'application/x-www-form-urlencoded',
